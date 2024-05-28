25beea1688c13ae4d0a749e515385019d012dda5
Hesam <hesam19hs@gmaile.com>
2024-05-28 14:51:44 +0330

    test4

---
+++ StringMethod.java
@@ -0,0 +1,9 @@
+public class StringMethod {
+    public static void main(String[] args) {
+        String Name1 = "Nima";
+        String Name2 = new String("Mehdi");
+
+        System.out.println(Name1);
+        System.out.println(Name2);
+    }
+}
