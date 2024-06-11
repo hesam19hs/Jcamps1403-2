425a0c0688dedb2370e41f32e3b48e0639b386a3
Hesam <hesam19hs@gmaile.com>
2024-06-11 15:09:17 +0330

    Methods2

--- Methods.java
+++ Methods.java
@@ -1,13 +1,27 @@
+import static java.lang.Math.sqrt;
+
 public class Methods {
     public static void main(String[] args) {
-        //int i = calculate(2, 5);
-        //System.out.println();
-        //process();
+        /*int i = calculate(2, 5);
+        System.out.println();
+        process();
         int i = age(1370, 1403);
-        System.out.println(i);
+        System.out.println(i);*/
+
+        System.out.println(Moraba(12));
+        System.out.println(Jazr());
     }
-    public static Integer age(int a, int b) {
+    /*public static Integer age(int a, int b) {
         return b - a;
+    }*/
+
+    public static Integer Moraba(int a){
+        int a2 = a * a;
+        return a2;
+    }
+    public static double Jazr(){
+        double b = sqrt(Moraba(12));
+        return b;
     }
 
     /*public static Integer calculate(int a, int b) {
