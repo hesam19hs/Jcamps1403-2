0f86528ae915944d9bd84d10195c9873039d0103
Hesam <hesam19hs@gmaile.com>
2024-06-11 14:56:35 +0330

    Methods

---
+++ Methods.java
@@ -0,0 +1,19 @@
+public class Methods {
+    public static void main(String[] args) {
+        //int i = calculate(2, 5);
+        //System.out.println();
+        //process();
+        int i = age(1370, 1403);
+        System.out.println(i);
+    }
+    public static Integer age(int a, int b) {
+        return b - a;
+    }
+
+    /*public static Integer calculate(int a, int b) {
+        return a + b;
+    }
+    public static void process() {
+        System.out.println(calculate(4, 7));
+    }*/
+}
