6d4dbf3b4b96046cdd410476b529cd2d93cf6eb0
Hesam <hesam19hs@gmaile.com>
2024-07-02 15:42:38 +0330

    package3

--- Learning/Book.java
+++ Learning/Book.java
@@ -1,21 +1,30 @@
 package Learning;
 import java.util.Date;
 public class Book {
+    public static void main(String[] args) {
+
+    }
     private String title;
     private String author;
     private String press;
     private String releaseDate;
 public void dispalyInfo(){
-    System.out.println("k");
+    System.out.println(
+            "title: '" + title +
+            "', author: '" + author +
+            "', press: '" + press +
+            "', releaseDate: '" + releaseDate + "\'"
+            );
 }
     public Book(String title, String author, String press, String releaseDate) {
-        this.title = title;
-        this.author = author;
+        this(title, author);
         this.press = press;
         this.releaseDate = releaseDate;
     }
 
     public Book() {
+        this.title = title;
+        this.author = author;
     }
 
 }
--- Learning/Learn.java
+++ Learning/Learn.java
@@ -1,4 +1,8 @@
 package Learning;
-
+import Book.*;
 public class Learn {
+    public static void main(String[] args) {
+        Book harryPotter = new Book("Harry", "jk");
+        harryPotter.dispalyInfo();
+    }
 }
