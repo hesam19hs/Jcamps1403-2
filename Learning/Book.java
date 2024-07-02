ac69c4714cfa8576180adef6a25b26778361a5e4
Hesam <hesam19hs@gmaile.com>
2024-07-02 14:59:58 +0330

    package2

--- Learning/Book.java
+++ Learning/Book.java
@@ -5,4 +5,17 @@ public class Book {
     private String author;
     private String press;
     private String releaseDate;
+public void dispalyInfo(){
+    System.out.println("k");
+}
+    public Book(String title, String author, String press, String releaseDate) {
+        this.title = title;
+        this.author = author;
+        this.press = press;
+        this.releaseDate = releaseDate;
+    }
+
+    public Book() {
+    }
+
 }
